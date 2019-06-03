6c8257f1b403403af2c69c6e49c0bb1d7642f68d
Marcel <marcel.poelzl@hotmail.com>
2019-06-03 14:53:18 +0200

    updated JavaFX

---
+++ flightLogFXML/src/Controller/MasterWindowController.java
@@ -0,0 +1,42 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MasterWindowController implements Initializable {
+
+    @FXML
+    private Button addFlightButton;
+
+    @FXML
+    private Button showLogButton;
+
+    @FXML
+    private TextField parameterTextField;
+
+    @FXML
+    void addFlightButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void parameterTextFieldHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showLogButtonHandler(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
