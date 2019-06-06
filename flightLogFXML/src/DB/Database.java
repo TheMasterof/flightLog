1c42c95c26510730e7ffdeba93ddcd37336edc9c
Marcel <marcel.poelzl@hotmail.com>
2019-06-06 11:38:27 +0200

    added Basic structure to classes Drone and User

--- flightLogFXML/src/DB/Database.java
+++ flightLogFXML/src/DB/Database.java
@@ -4,6 +4,9 @@ import Model.Drone;
 import Model.LogEntry;
 import Model.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,11 +14,19 @@ public class Database {
     //region Fields
     private Database instance;
     private List<LogEntry> logs;
+    Connection connection;
     //endregion
 
     //region Constructor
     private Database(){
         logs = new LinkedList<>();
+        try {
+            connection = DriverManager.getConnection("jdbc:mariadb://192.168.99.100:3306/mydb", "user", "passme");
+            System.out.println("Connection OK!");
+        } catch (SQLException e) {
+            System.out.println("Verbindung zur DB nicht möglich");
+            e.printStackTrace();
+        }
     }
         public Database getInstance(){
         if(instance == null)instance = new Database();
--- flightLogFXML/src/Model/Drone.java
+++ flightLogFXML/src/Model/Drone.java
@@ -1,4 +1,57 @@
 package Model;
 
 public class Drone {
+    //region FIELDS
+    private int id;
+    private String name;
+    private String description;
+    private Boolean available;
+    //endregion
+
+    //region CONSTRUCTOR
+
+    public Drone(int id, String name, String description, Boolean available) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.available = available;
+    }
+
+    //endregion
+
+    //region GetterSetter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    //endregion
 }
