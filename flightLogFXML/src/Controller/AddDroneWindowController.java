bae28447e33c27d81eb30efe52953c0c61b75e21
Alex <kapsammeralex@gmail.com>
2019-06-11 12:38:56 +0200

    changed addDrone fxml file

--- flightLogFXML/src/Controller/AddDroneWindowController.java
+++ flightLogFXML/src/Controller/AddDroneWindowController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -23,7 +24,7 @@ public class AddDroneWindowController implements Initializable {
     private TextField nameTextField;
 
     @FXML
-    private TextField descriptionTextField;
+    private TextArea descriptionTextField;
 
     @FXML
     private Button addButton;
